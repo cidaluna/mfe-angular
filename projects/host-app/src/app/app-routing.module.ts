5bb71fa31ab59ac57873fb198ecdc2a07481148f
Cida Luna <cidalunna@hotmail.com>
2024-10-02 19:34:54 -0300

    Start host and mfe applications

---
+++ projects/host-app/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'todo-list', component: TodoComponent},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+export const routeCompArr = [HomeComponent, TodoComponent];
